02c90c7cd07c7cbaaeecd6f7d6b8e1995a9a1d3e
betz <betzinbal@gmail.com>
2024-11-10 11:37:44 +0200

    base base express for base stracther

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
+import express from 'express'
+import 'dotenv'
 
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.listen(PORT, ()=>{
+    console.log(`server run visit http://localhost:${PORT}`);
+    
+})
